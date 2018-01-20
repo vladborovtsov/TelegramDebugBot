f5f4e8571e16739d2e40e3728c23b2e3b3e17f32
Vlad Borovtsov <borovcov.vlad@gmail.com>
2018-01-20 17:01:33 +0700

    send file endpoint

--- api.py
+++ api.py
@@ -6,6 +6,9 @@ import config as CONFIG
 from flask import Flask, jsonify, abort, request
 from telegram.bot import Bot
 import chats_storage
+import tempfile
+import base64
+import os
 
 
 bot = Bot(CONFIG.TELEGRAM_BOT_TOKEN)
@@ -18,7 +21,6 @@ def root():
 
 @app.route('/send-formatted', methods=['GET', 'POST'])
 def send_formatted():
-    print(request.values)
     if request.json:
         try:
             caption = request.json["caption"]
@@ -33,6 +35,36 @@ def send_formatted():
 
         for cid in chats_storage.AllChatIds():
             bot.send_message(cid, parse_mode=telegram.ParseMode.HTML, text="<b>{}</b>\n<pre>{}</pre>".format(caption,text))
+        return jsonify({"status" : "ok"})
+    abort(400)
+
+
+@app.route("/send-file", methods=['POST',])
+def send_file():
+    if request.json:
+        try:
+            text = request.json['text']
+            secret = request.json["secret"]
+            file_base64 = request.json["data"]
+            file_ext = request.json["ext"]
+        except KeyError as e:
+            print(e)
+            abort(400)
+
+        if secret != CONFIG.REST_SECRET:
+            abort(403)
+
+        for cid in chats_storage.AllChatIds():
+            f = tempfile.NamedTemporaryFile("w+b", suffix=".{}".format(file_ext))
+            data = base64.decodebytes(file_base64.encode('utf8'))
+            f.write(data)
+            f.seek(0)
+
+            bot.send_document(chat_id=cid, document=f, filename=os.path.basename(f.name), caption=text)
+
+            f.close()
+            
+
         return jsonify({"status" : "ok"})
     abort(400)
 
